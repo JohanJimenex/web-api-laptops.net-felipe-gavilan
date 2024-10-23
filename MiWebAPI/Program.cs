0250155779d95e929dd5797fbac0356006412efc
johanjimenex <johan.jimenez.23@gmail.com>
2024-10-23 19:54:08 -0400

    Refactor CORS configuration and add README documentation

--- MiWebAPI/Program.cs
+++ MiWebAPI/Program.cs
@@ -3,38 +3,32 @@ using MiWebAPI;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-var origenesPermitidos = builder.Configuration.GetValue<string>("OrigenesPermitidos")!.Split(",");
-
-builder.Services.AddCors(opciones =>
-{
-    opciones.AddDefaultPolicy(politica =>
-    {
+// Configuracion de CORS
+builder.Services.AddCors(opciones => {
+    opciones.AddDefaultPolicy(politica => {
+        //obtenemos los valores de el appsettings.json si hay varios dibidiospor coma
+        var origenesPermitidos = builder.Configuration.GetValue<string>("OrigenesPermitidos")!.Split(",");
         politica.WithOrigins(origenesPermitidos).AllowAnyHeader().AllowAnyMethod();
     });
 });
 
+// inyectamos el contexto de la base de datos para poder usarlo en los controladores de la siguiente forma: 
+// private readonly ApplicationDbContext _context;
+// public MiNombreController(ApplicationDbContext context)
+// {
+//     _context = context;
+// }
 builder.Services.AddDbContext<ApplicationDbContext>(opciones => opciones.UseSqlServer("name=DefaultConnection"));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
+if (app.Environment.IsDevelopment()) { app.MapOpenApi(); }
 app.UseHttpsRedirection();
-
-app.UseCors();
-
+app.UseCors();//agregamos el cors
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
---
+++ README.md
@@ -0,0 +1,16 @@
+## Descripción
+
+En este proyecto de **Felipe Gavilan** implementamos:
+
+### Funcionalidades
+
+1. 🔗 **Conexión a SQL Server con Entity Framework y Code First**
+   - Configuración de la cadena de conexión
+   - Creación de modelos y migraciones
+   - Actualización de la base de datos
+
+2. 🔒 **Configuraciones de CORS**
+   - Permitir solicitudes de orígenes específicos
+   - Configuración de políticas de CORS
+   - Implementación en el middleware de ASP.NET Core
+  
